54483848e982ab8d008aa644b7852f5835b8e0f0
Ubuntu <ubuntu@ip-172-31-44-171.us-west-2.compute.internal>
2013-07-27 18:51:06 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,13 +2,16 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+/*
 var data = new Buffer(25);
 data = fs.readFileSync('/index.html')
 data = data.toString("utf-8");
+*/
+
 
 
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send("Hello World, I am born again!!");
 });
 
 
